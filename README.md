4b6b2cbe8d41ba7b3a18a6699289a7a046e8728f
Daniil Sukhochev <sushidze@gmail.com>
2022-12-25 22:11:17 +0000

    Translate project to English

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Модуль фитнес-трекера
+# Fitness tracker module
+
+A software module for a fitness tracker using the OOP paradigm. This module calculates and display the results of the workout.
+It performs the following functions:
+```
+1. Recieve information about past training
+2. Determine type of training
+3. Calculate result of training
+4. Show information message with result of the training
+```
+
--- homework.py
+++ homework.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class InfoMessage:
-    """Информационное сообщение о тренировке."""
+    """Information message about training."""
 
     training_type: str
     duration: float
@@ -12,19 +12,19 @@ class InfoMessage:
     calories: float
 
     def get_message(self) -> str:
-        """Генерация сообщения."""
+        """Generate message."""
         info = (
-            f"Тип тренировки: {self.training_type}; "
-            f"Длительность: {round(self.duration,3):.3f} ч.; "
-            f"Дистанция: {round(self.distance,3):.3f} км; "
-            f"Ср. скорость: {round(self.speed,3):.3f} км/ч; "
-            f"Потрачено ккал: {round(self.calories,3):.3f}."
+            f"Training type: {self.training_type}; "
+            f"Duration: {round(self.duration,3):.3f} h.; "
+            f"Distance: {round(self.distance,3):.3f} km; "
+            f"Av. speed: {round(self.speed,3):.3f} km/h; "
+            f"Kcal: {round(self.calories,3):.3f}."
         )
         return info
 
 
 class Training:
-    """Базовый класс тренировки."""
+    """Base class of training."""
 
     LEN_STEP: float = 0.65
     M_IN_KM: int = 1000
@@ -41,21 +41,17 @@ class Training:
         self.weight = weight
 
     def get_distance(self) -> float:
-        """Получить дистанцию в км."""
         distance = self.action * self.LEN_STEP / self.M_IN_KM
         return distance
 
     def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
         speed = self.get_distance() / self.duration
         return speed
 
     def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
         pass
 
     def show_training_info(self) -> InfoMessage:
-        """Вернуть информационное сообщение о выполненной тренировке."""
         info = InfoMessage(
             self.__class__.__name__,
             duration=self.duration,
@@ -67,7 +63,7 @@ class Training:
 
 
 class Running(Training):
-    """Тренировка: бег."""
+    """Training type: running."""
 
     KOEFF_RUNNING_1: int = 18
     KOEFF_RUNNING_2: int = 20
@@ -88,7 +84,7 @@ class Running(Training):
 
 
 class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""
+    """Training type: sports walking."""
 
     KOEFF_WALKING_1: float = 0.035
     KOEFF_WALKING_2: float = 0.029
@@ -112,7 +108,7 @@ class SportsWalking(Training):
 
 
 class Swimming(Training):
-    """Тренировка: плавание."""
+    """Training type: swimming."""
 
     KOEFF_SWIMMING_1: float = 1.1
     KOEFF_SWIMMING_2: int = 2
@@ -139,7 +135,7 @@ class Swimming(Training):
 
 
 def read_package(workout_type: str, data: list) -> Training:
-    """Прочитать данные полученные от датчиков."""
+    """Read data, which was received from module."""
 
     sports = {'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking}
     sport = sports[workout_type](*data)
@@ -147,7 +143,6 @@ def read_package(workout_type: str, data: list) -> Training:
 
 
 def main(training: Training) -> None:
-    """Главная функция."""
     info = training.show_training_info().get_message()
     print(info)
 
